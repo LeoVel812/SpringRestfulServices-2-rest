1312b567d5d2a1e908d55f07b10b25fe60b4c224
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-11 22:45:32 -0600

    add scaffold

---
+++ src/main/java/com/lv/rest/SpringRestfulServices2RestApplication.java
@@ -0,0 +1,13 @@
+package com.lv.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestfulServices2RestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestfulServices2RestApplication.class, args);
+	}
+
+}
