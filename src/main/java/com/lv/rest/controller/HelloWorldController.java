24f4d1ef107378aa32493bebbf93e8f1dcb00058
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-11 23:05:10 -0600

    add a get endpoint with bean

--- src/main/java/com/lv/rest/controller/HelloWorldController.java
+++ src/main/java/com/lv/rest/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.lv.rest.controller;
 
+import com.lv.rest.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
         return "Hello there!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
     }
 
 }
