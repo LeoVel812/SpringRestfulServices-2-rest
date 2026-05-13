f2dd560061b6861584b9f27c02256a81d4609e8b
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-13 00:08:21 -0600

    API Versioning: Add Springboot impl Request Param Versioning

--- src/main/java/com/lv/rest/versioning/VersioningPersonSpringbootImplController.java
+++ src/main/java/com/lv/rest/versioning/VersioningPersonSpringbootImplController.java
@@ -9,22 +9,22 @@ public class VersioningPersonSpringbootImplController {
     private static final String NAME_V1 = "LeonV First";
     private static final String LASTNAME_V2 = "Second";
 
-    @GetMapping(value = "/{version}/person/sbi", version = "1.0.0")
-    public PersonV1 getFirstVersionOfPerson() {
-        return new PersonV1(NAME_V1);
-    }
+//    @GetMapping(value = "/{version}/person/sbi", version = "1.0.0")
+//    public PersonV1 getFirstVersionOfPerson() {
+//        return new PersonV1(NAME_V1);
+//    }
+//
+//    @GetMapping(value = "/{version}/person/sbi", version = "2.0.0")
+//    public PersonV2 getSecondVersionOfPerson() {
+//        return new PersonV2(new Name(NAME, LASTNAME_V2));
+//    }
 
-    @GetMapping(value = "/{version}/person/sbi", version = "2.0.0")
-    public PersonV2 getSecondVersionOfPerson() {
-        return new PersonV2(new Name(NAME, LASTNAME_V2));
-    }
-
-    @GetMapping(path = "/person/sbi", params = "version=1")
+    @GetMapping(path = "/person/sbi", version = "1.0.0")
     public PersonV1 getFirstVersionOfPersonRequestParam() {
         return new PersonV1(NAME_V1);
     }
 
-    @GetMapping(path = "/person/sbi", params = "version=2")
+    @GetMapping(path = "/person/sbi", version = "2.0.0")
     public PersonV2 getSecondVersionOfPersonRequestParam() {
         return new PersonV2(new Name(NAME, LASTNAME_V2));
     }
