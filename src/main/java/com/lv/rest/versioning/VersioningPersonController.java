c0103ba1fed2c5b4d6c75a252d898c8d46f3908f
Leonardo Velazquez <leovelazquez812@gmail.com>
2026-05-12 23:55:14 -0600

    API Versioning: Add Springboot impl URI Versioning

--- src/main/java/com/lv/rest/versioning/VersioningPersonController.java
+++ src/main/java/com/lv/rest/versioning/VersioningPersonController.java
@@ -5,43 +5,47 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersioningPersonController {
+    private static final String NAME = "LeonV";
+    private static final String NAME_V1 = "LeonV First";
+    private static final String LASTNAME_V2 = "Second";
+
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson() {
-        return new PersonV1("LeonV First");
+        return new PersonV1(NAME_V1);
     }
 
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson() {
-        return new PersonV2(new Name("LeonV", "Second"));
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
     }
 
     @GetMapping(path = "/person", params = "version=1")
     public PersonV1 getFirstVersionOfPersonRequestParam() {
-        return new PersonV1("LeonV First");
+        return new PersonV1(NAME_V1);
     }
 
     @GetMapping(path = "/person", params = "version=2")
     public PersonV2 getSecondVersionOfPersonRequestParam() {
-        return new PersonV2(new Name("LeonV", "Second"));
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
     }
 
     @GetMapping(path = "/person", headers = "X-API-VERSION=1")
     public PersonV1 getFirstVersionOfPersonRequestHeader() {
-        return new PersonV1("LeonV First");
+        return new PersonV1(NAME_V1);
     }
 
     @GetMapping(path = "/person", headers = "X-API-VERSION=2")
     public PersonV2 getSecondVersionOfPersonRequestHeader() {
-        return new PersonV2(new Name("LeonV", "Second"));
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
     }
 
     @GetMapping(path = "/person", produces = "application/lv.business.app-v1+json")
     public PersonV1 getFirstVersionOfPersonAcceptHeader() {
-        return new PersonV1("LeonV First");
+        return new PersonV1(NAME_V1);
     }
 
     @GetMapping(path = "/person", produces = "application/lv.business.app-v2+json")
     public PersonV2 getSecondVersionOfPersonAcceptHeader() {
-        return new PersonV2(new Name("LeonV", "Second"));
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
     }
 }
---
+++ src/main/java/com/lv/rest/versioning/VersioningPersonSpringbootImplController.java
@@ -0,0 +1,32 @@
+package com.lv.rest.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonSpringbootImplController {
+    private static final String NAME = "LeonV";
+    private static final String NAME_V1 = "LeonV First";
+    private static final String LASTNAME_V2 = "Second";
+
+    @GetMapping(value = "/{version}/person/sbi", version = "1.0.0")
+    public PersonV1 getFirstVersionOfPerson() {
+        return new PersonV1(NAME_V1);
+    }
+
+    @GetMapping(value = "/{version}/person/sbi", version = "2.0.0")
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
+    }
+
+    @GetMapping(path = "/person/sbi", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParam() {
+        return new PersonV1(NAME_V1);
+    }
+
+    @GetMapping(path = "/person/sbi", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParam() {
+        return new PersonV2(new Name(NAME, LASTNAME_V2));
+    }
+
+}
